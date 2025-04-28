c90511cd157925c247f919a49d28242eb4fb38b6
NicoDuchesne <145458654+NicoDuchesne@users.noreply.github.com>
2025-04-28 08:20:48 +0200

    First commit

---
+++ PortfolioWebsite/js/swiper.js
@@ -0,0 +1,12 @@
+"use strict";//Returns all errors
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
